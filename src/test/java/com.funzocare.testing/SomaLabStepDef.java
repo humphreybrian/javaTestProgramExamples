b7b5e01cbe46ae5ca3820267f9b7ec009f8ca03a
humphreybrian <developer.humphrey@gmail.com>
2021-04-01 01:31:46 +0300

    new change

--- src/test/java/com.funzocare.testing/SomaLabStepDef.java
+++ src/test/java/com.funzocare.testing/SomaLabStepDef.java
@@ -4,11 +4,12 @@ import cucumber.api.java8.En;
 
 public class SomaLabStepDef implements En {
     public SomaLabStepDef() {
+        Given("^You have given the valid number$", () -> {
+        });
         When("^I play with number (\\d+)$", (Integer arg0) -> {
         });
         Then("^The result was Walaaah$", () -> {
         });
-        Given("^You have given the valid number$", () -> {
-        });
+
     }
 }
